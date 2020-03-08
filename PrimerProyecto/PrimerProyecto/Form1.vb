553e070cf2c19ac73e5b92efcd61fd43093a9b3d
Daniel Díaz <dandia28@gmail.com>
2020-03-08 20:35:11 -0300

    Add files via upload

---
+++ PrimerProyecto/PrimerProyecto/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmMain
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        MsgBox("Si puedes leer esto es porque descargaste correctamente de GitHub El Proyecto")
+    End Sub
+End Class
